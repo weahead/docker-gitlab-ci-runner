755ba2ce4900f633e2030db8cf23f1005cd409db
Jonathan <jonathan.borg@weahead.se>
2017-02-21 19:19:16 +0100

    omg such a ugly hack just to get env variables injected into build runners, the gitlab-ci-runner does not allow multiple values in RUNNER_ENV variable..

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,17 @@
 
 export CI_USER=gitlab-runner
 /set-docker-group.sh
-if [[ ! -f /etc/gitlab-runner/config/config.toml ]]; then /usr/bin/gitlab-runner register; fi
+if [[ ! -f /etc/gitlab-runner/registered ]]; then
+touch /etc/gitlab-runner/registered
+register_cmd="/usr/bin/gitlab-runner register \
+  -n \
+  -r ${TOKEN} \
+  -name ${NAME} \
+  -u ${URL} \
+  --executor ${EXECUTOR} \
+  --docker-image ${BASE_IMAGE} \
+  --env ${ENV_VARS//' '/' --env '} \
+  --docker-privileged true"
+$register_cmd
+fi
 /usr/bin/gitlab-runner --debug run --user=${CI_USER} --working-directory=/home/${CI_USER}
